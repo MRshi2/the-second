42d64348c5289581f08b35c2fa8afd1c8c61f6a6
MRshi2 <2574016161@qq.com>
2017-11-03 23:02:12 +0800

    ''

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,8 +4,8 @@
  		<swiper-content/>
  		<shi-content/>
  		<!--<home-Listitem></home-Listitem>-->
- 		<mpxwkcontent/>
- 		<week-content/>
+ 		<!--<mpxwkcontent/>
+ 		<week-content/>-->
  	</div>
 </template>
 
@@ -14,9 +14,7 @@
 import HeaderComponent from "./Header";
 import SwiperComponent from "./Swiper";
 import ShiserComponent from "./Shi";//石文渊
-<<<<<<< HEAD
-/*import ListitemComponent from './Listitem';//路雪缘
-=======
+/*
 //import ListitemComponent from './Listitem';//路雪缘
 >>>>>>> d1b2d4b4b5c28a410e7b8bfef16c6d2d11dc86bb
 import MpXwkcotaintList from "./Xwklist";//徐文凯
@@ -26,8 +24,8 @@ export default {
 	components: {
 		"home-header": HeaderComponent,
 		"swiper-content": SwiperComponent,
-		"shi-content":ShiserComponent,
-<<<<<<< HEAD
+		"shi-content":ShiserComponent
+
 /*		'home-Listitem' : ListitemComponent,
 =======
 //		'home-Listitem' : ListitemComponent,
