5b836d32c57f4ec74754acac24c0b70e7cec7219
Perfectxu <467979161@qq.com>
2017-11-04 09:41:46 +0800

    change home

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,8 +4,8 @@
  		<swiper-content/>
  		<shi-content/>
  		<!--<home-Listitem></home-Listitem>-->
- 		<!--<mpxwkcontent/>
- 		<week-content/>-->
+ 		<mpxwkcontent/>
+ 		<week-content/>
  	</div>
 </template>
 
@@ -14,24 +14,18 @@
 import HeaderComponent from "./Header";
 import SwiperComponent from "./Swiper";
 import ShiserComponent from "./Shi";//石文渊
-/*
 //import ListitemComponent from './Listitem';//路雪缘
->>>>>>> d1b2d4b4b5c28a410e7b8bfef16c6d2d11dc86bb
 import MpXwkcotaintList from "./Xwklist";//徐文凯
-import weekList from './weekList';//赵振平*/
+import weekList from './weekList';//赵振平
 
 export default {
 	components: {
 		"home-header": HeaderComponent,
 		"swiper-content": SwiperComponent,
-		"shi-content":ShiserComponent
-
-/*		'home-Listitem' : ListitemComponent,
-=======
+		"shi-content":ShiserComponent,
 //		'home-Listitem' : ListitemComponent,
->>>>>>> d1b2d4b4b5c28a410e7b8bfef16c6d2d11dc86bb
 		"mpxwkcontent":MpXwkcotaintList,
-		"week-content": weekList*/
+		"week-content": weekList
 		
 	}
 }
