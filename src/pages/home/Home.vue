a416b9c2c50903d649b98383a4024ab0b5ce9a6d
NbZzp <985736882@qq.com>
2017-11-04 09:55:22 +0800

    a

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -12,32 +12,21 @@
 <script>
 
 import HeaderComponent from "./Header";
-import SwiperComponent from "./Swiper";
+ import SwiperComponent from "./Swiper";
 import ShiserComponent from "./Shi";//石文渊
-<<<<<<< HEAD
 // import ListitemComponent from './Listitem';//路雪缘
-=======
-//import ListitemComponent from './Listitem';//路雪缘
->>>>>>> 5b836d32c57f4ec74754acac24c0b70e7cec7219
 import MpXwkcotaintList from "./Xwklist";//徐文凯
 import weekList from './weekList';//赵振平
 
 export default {
 	components: {
+		
 		"home-header": HeaderComponent,
-		"swiper-content": SwiperComponent,
+		 "swiper-content": SwiperComponent,
 		"shi-content":ShiserComponent,
-<<<<<<< HEAD
 		// 'home-Listitem' : ListitemComponent,
 		"mpxwkcontent":MpXwkcotaintList,
 		"week-content": weekList
-
-
-=======
-//		'home-Listitem' : ListitemComponent,
-		"mpxwkcontent":MpXwkcotaintList,
-		"week-content": weekList
->>>>>>> 5b836d32c57f4ec74754acac24c0b70e7cec7219
 		
 	}
 }
