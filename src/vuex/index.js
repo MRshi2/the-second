ac99f2e74684a9edb478243e4c605dccba2f47dc
MRshi2 <2574016161@qq.com>
2017-11-10 09:52:12 +0800

    'fang'

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -5,6 +5,7 @@ import lunbo from '../pages/detail-shi-lunbo/module.js'
 import detail from '../pages/detail-shi/module.js'
 import city from '../pages/city/module.js'
 import onedaytour from '../pages/onedaytour/module.js'
+import oneday from "../pages/oneday/Components/Component/module.js"
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -14,6 +15,8 @@ export default new Vuex.Store({
 		detail:detail,
 		city:city,
 		onedaytour:onedaytour,
+		nav: oneday,
+		datagoInfo: oneday
 		
 	}
 })
